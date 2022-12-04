ae059918936618012c07d0c47497a582c4b24ab6
jchassaing <jeremie.chassaing@gmail.com>
2022-12-04 01:28:37 +0100

    Better solution for day 3

--- day03.fsx
+++ day03.fsx
@@ -1,15 +1,10 @@
-
 let parse (l: string) = 
     let half = l.Length/2
     l.Substring(0,half), l.Substring(half)
 
 let findCommonItem (x:string,y: string) =
-    [ for cx in x do
-      for cy in y do
-        if cx = cy then
-            cx]
-    |> List.distinct
-
+    x |> Seq.find (fun c -> y.Contains(c))
+            
 let score (c: char) =
     if c >= 'a' && c <= 'z' then
         int c - int 'a' + 1
@@ -18,20 +13,11 @@ let score (c: char) =
 
 
 System.IO.File.ReadAllLines("input/day03.txt")
-|> Seq.collect(parse>>findCommonItem)
-|> Seq.sumBy score
+|> Seq.sumBy(parse>>findCommonItem>> score)
 
-let findBadge [|x : string;y;z|] =
-    [ for cx in x do
-      for cy in y do
-      for cz in z do
-        if cx = cy && cx = cz then
-            cx
-    ]
-    |> List.distinct
-
-
+let findBadge [|x : string;y: string;z: string|] =
+    x |> Seq.find (fun c -> y.Contains(c) && z.Contains(c))
+    
 System.IO.File.ReadAllLines("input/day03.txt")
 |> Seq.chunkBySize 3
-|> Seq.collect findBadge
-|> Seq.sumBy score
+|> Seq.sumBy (findBadge >> score)
