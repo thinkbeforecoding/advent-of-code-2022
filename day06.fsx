ff27a8d7c83370a7b7616009c18483e54c713acf
jchassaing <jeremie.chassaing@gmail.com>
2022-12-06 11:23:20 +0100

    Day 6: simpler version

---
+++ day06.fsx
@@ -0,0 +1,22 @@
+open System
+
+let rec allDiff (span: _ ReadOnlySpan) x  =
+    if x < span.Length - 1 then
+        if span.Slice(x+1).Contains(span[x]) then
+            false
+        else
+            allDiff span (x+1)
+    else
+        true
+
+let rec find i l (txt: string)  =
+    let chars = txt.AsSpan().Slice(i,l)
+    if allDiff chars 0  then
+        i+l
+    else
+       find (i+1) l txt
+
+System.IO.File.ReadAllText("input/day06.txt")
+|> find 0 4
+System.IO.File.ReadAllText("input/day06.txt")
+|> find 0 14
